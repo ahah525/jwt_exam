24f3f3ebbee76db66aa3a5036f1185bb8a5ca4ca
ahah525 <ahah525@naver.com>
2022-10-04 14:26:14 +0900

    chore : jwt 관련 라이브러리 추가, test : 시크릿키 원문을 HMAC 암호화 방식으로 SecretKey 객체 생성

--- src/test/java/com/ll/exam/app__2022_10_04/JwtTests.java
+++ src/test/java/com/ll/exam/app__2022_10_04/JwtTests.java
@@ -1,20 +1,33 @@
 package com.ll.exam.app__2022_10_04;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtTests {
 	@Value("${custom.jwt.secretKey}")
-	private String secretKey;
+	private String secretKeyPlain;		// 시크릿키 원문
 
 	@Test
 	@DisplayName("secretKey 키가 존재해야 한다.")
 	void t1() {
+		assertThat(secretKeyPlain).isNotNull();
+	}
+
+	@Test
+	@DisplayName("secretKey 원문으로 hmac 암호화 알고리즘에 맞는 secretKey 객체를 만들 수 있어야 한다.")
+	void t2() {
+		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+
 		assertThat(secretKey).isNotNull();
 	}
 }
