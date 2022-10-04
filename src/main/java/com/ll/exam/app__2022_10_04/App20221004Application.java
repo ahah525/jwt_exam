2d1e6b184a6fadbf5215f73bcf2e3807f4e90e43
ahah525 <ahah525@naver.com>
2022-10-04 15:10:44 +0900

    feat : SecretKey 캐싱(EnableCaching, Cacheable)

--- src/main/java/com/ll/exam/app__2022_10_04/App20221004Application.java
+++ src/main/java/com/ll/exam/app__2022_10_04/App20221004Application.java
@@ -2,7 +2,9 @@ package com.ll.exam.app__2022_10_04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class App20221004Application {
 
--- src/test/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
+++ src/test/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
@@ -2,6 +2,7 @@ package com.ll.exam.app__2022_10_04.app.jwt;
 
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -12,6 +13,7 @@ public class JwtProvider {
     @Value("${custom.jwt.secretKey}")
     private String secretKeyPlain;		// 시크릿키 원문
 
+    @Cacheable(value = "secretKey")
     public SecretKey getSecretKey() {
         String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
         SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
